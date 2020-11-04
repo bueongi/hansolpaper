dc0ac0ea7867f35397e39befdda4bf2f261b4abe
bueongi <67132017+bueongi@users.noreply.github.com>
2020-11-04 14:27:47 +0900

    Add files via upload

--- js/default.js
+++ js/default.js
@@ -26,12 +26,56 @@ $('.lang>a').on('click',function(){
 
 // 메인네비 클릭
 $('.main_nav>li>a').on('click',function(){
-  if($(this).hasClass('active')){
-    $(this).removeClass('active');
-    $(this).next().slideUp(300);
-  }else{
-    $(this).addClass('active');
-    $(this).next().slideDown(300);
-  }
+  var winWidth = $(window).width();
+  if(winWidth<1280){
+    if($(this).hasClass('active')){
+      var winWidth = $(window).width();
+      $(this).removeClass('active');
+      $(this).next().slideUp(300);
+    }else{
+      closeSub();
+      $(this).addClass('active');
+      $(this).next().slideDown(300);
+    };
+  };
 })
+
+//윈도우 리사이즈 이벤트
+$(window).on('resize',function(){
+  var winWidth = $(window).width();
+  if(winWidth>=768){
+    closeSub();
+  }
+});
+
+// 서브메뉴 닫기
+function closeSub(){
+  $('.main_nav>li>a').removeClass('active');
+  $('.main_nav>li>a').next().slideUp();
+};
+
+//문서 해드 스크롤
+$(document).on('scroll',function(){
+  var docTop = $(window).scrollTop();
+  var hrd = $('.header');
+  if(docTop>0){
+    hrd.addClass('active');
+  }else{
+    hrd.removeClass('active');
+  }
+});
+
+
+
+
+//모바일 서브메뉴 호출/닫기
+// function moSub(self){
+//   if($(self).hasClass('active')){
+//     $(self).removeClass('active');
+//     $(self).next().slideUp(300);
+//   }else{
+//     $(self).addClass('active');
+//     $(self).next().slideDown(300);
+//   };
+// }
 
